9796c1e72f8a6c7ddee1939faacdac5b22f2e4af
fabianko <fabian.arismendi@usach.cl>
2020-06-13 21:16:44 -0400

    FIX: progress-bar style

--- src/app/cv/dtos/cvClassDto.ts
+++ src/app/cv/dtos/cvClassDto.ts
@@ -1,5 +1,6 @@
 import { ProfessionalExperienceDto } from './ProfessionalExperienceDto';
 import { StudyDto } from './StudyDto';
+import { skillDto } from './skillDto';
 
 export class cvClassDto {
   lateral1: string;
@@ -7,11 +8,14 @@ export class cvClassDto {
   lateral1Text: string;
   lateral3:string;
   profession:string;
-  title1:string;
-  title2:string;
+  title1: string;
+  title2: string;
+  title3: string;
   experiences: ProfessionalExperienceDto[];
   studiesList: StudyDto[];
   tags: tag[];
+  skills: skillDto[];
+  lastUpdate: string;
 }
 export class tag {
   name: string;
