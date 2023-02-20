14a3b1c2675ef34a80fc885272baf1ba0959534b
Fabianko <fabian.arismendi@usach.cl>
2023-02-19 22:37:12 -0300

    FIX: game tic tac toe

--- src/app/tictactoegame/game/game.component.ts
+++ src/app/tictactoegame/game/game.component.ts
@@ -17,11 +17,15 @@ export class GameTicTacToeComponent implements OnInit {
 
 
   validWin(e: boardClass) {
+    let validateHorizontal = this.validHorizontalWin(e.board, e.nowPlayer);
+    let validateVertical = this.validVerticalWin(e.board, e.nowPlayer);
+    let validateDiagonal1 = this.validDiagonal1(e.board, e.nowPlayer);
+    let validateDiagonal2 = this.validDiagonal2(e.board, e.nowPlayer);
     if (
-      this.validHorizontalWin(e.board, e.nowPlayer) ||
-      this.validVerticalWin(e.board, e.nowPlayer) ||
-      this.validDiagonal1(e.board, e.nowPlayer) ||
-      this.validDiagonal2(e.board, e.nowPlayer)
+      validateHorizontal ||
+      validateVertical ||
+      validateDiagonal1 ||
+      validateDiagonal2
     ) {
       this.winner = e.nowPlayer;
       return true;
@@ -32,43 +36,42 @@ export class GameTicTacToeComponent implements OnInit {
   }
 
 
-  public validHorizontalWin(board, player): boolean {
-    let i = 0;
-    let j = 0;
-    while (i < board.length) {
-      if (board[i][j] == player) {
-        if (j == board.length - 1) {
-          return true;
+  public validHorizontalWin(board: number[][], player: number): boolean {
+    for (let i = 0; i < board.length; i++) {
+      let j = 0;
+      while (j < board[i].length) {
+        if (board[i][j] == player) {
+          if (j == board[i].length - 1) {
+            return true;
+          }
+          j++;
+        } else {
+          break;
         }
-        j++;
       }
-      else {
-        i++;
+    }
+    return false;
+  }
+  
+  public validVerticalWin(board: number[][], player: number): boolean {
+    for (let j = 0; j < board.length; j++) {
+      let i = 0;
+      while (i < board[j].length) {
+        if (board[i][j] == player) {
+          if (i == board[j].length - 1) {
+            return true;
+          }
+          i++;
+        } else {
+          break;
+        }
       }
     }
     return false;
   }
 
 
-  public validVerticalWin(board, player): boolean {
-    let i = 0;
-    let j = 0;
-    while (j < board.length) {
-      if (board[i][j] == player) {
-        if (i == board.length - 1) {
-          return true;
-        }
-        i++;
-      }
-      else {
-        j++;
-      }
-    }
-    return false;
-  }
-
-
-  public validDiagonal1(board, player): boolean {
+  public validDiagonal1(board: number[][], player: number): boolean {
     let i = 0;
     let j = 0;
     while (j < board.length) {
@@ -84,7 +87,7 @@ export class GameTicTacToeComponent implements OnInit {
   }
 
 
-  public validDiagonal2(board, player): boolean {
+  public validDiagonal2(board: number[][], player: number): boolean {
     let i = 0;
     let j = board.length - 1;
     while (i < board.length) {
