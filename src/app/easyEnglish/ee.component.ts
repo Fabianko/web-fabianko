7e45adcd318405f5f9b65e4a077f5b960795255d
Fabianko <fabian.arismendi@usach.cl>
2023-06-21 10:57:39 -0400

    ADD: examples and component's corrections

--- src/app/easyEnglish/ee.component.ts
+++ src/app/easyEnglish/ee.component.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit,Pipe } from '@angular/core';
+import { Component, OnInit, Pipe } from '@angular/core';
 import * as data from '../../assets/api/tips/tips.json';
 
 @Component({
   selector: 'ee',
   templateUrl: './ee.component.html',
-  styleUrls: ['./ee.component.less']
+  styleUrls: ['./ee.component.less'],
 })
 export class EeComponent implements OnInit {
-    selected: any
-    constructor() { }
+  selected: any;
+  data: any;
+  constructor() {}
 
-    ngOnInit() {
-      this.random()
+  ngOnInit() {
+    this.data = data['default'];
+    this.random();
+  }
+  public random() {
+    if (this.selected) {
+      //eliminar el objeto seleccionado de data, para que no se repita
+      this.data = this.data.filter((item) => item !== this.selected);
+      this.selected = undefined;
     }
-    public random(){
-        this.selected = undefined
-        setTimeout(() => {
-            this.selected = data['default'][Math.floor(Math.random() * data['default'].length)]
-        }, 0);
-        
-    }
+    setTimeout(() => {
+      this.selected =
+        this.data[Math.floor(Math.random() * this.data.length)];
+    }, 0);
+  }
+  public reloadData() {
+    this.data = data['default'];
+    this.random();
+  }
 }
