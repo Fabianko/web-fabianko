2ab21ea9195ecd510a525415582323764fbf729f
Fabianko <fabian.arismendi@usach.cl>
2023-06-25 21:46:31 -0400

    ADD: Feedback btn

--- src/app/easyEnglishHome/home.component.ts
+++ src/app/easyEnglishHome/home.component.ts
@@ -11,8 +11,7 @@ export class EeHomeComponent implements OnInit {
     { name: 'Verbal Tenses', route: 'verbalTenses' },
     { name: 'Conditional sentences', route: 'conditionalSentences' },
     { name: 'Phrasal verbs', route: 'phrasalVerbs' },
-    { name: 'Idioms', route: 'tips' },
-    { name: 'Collocations', route: 'tips' }
+    
   ];
   constructor() {}
 
