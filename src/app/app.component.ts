07404dba483ebc6afdfedd9ce19ee73ea3698ef9
Fabianko <fabian.arismendi@usach.cl>
2023-10-11 22:29:11 -0300

    ADD: dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-mrk';
+  icon = 'assets/img/sun.png';
+  iconMoon = 'assets/img/moon.png';
+  iconSun = 'assets/img/sun.png';
+  status = false;
+  toggleTheme() {
+    if (this.status) {
+      document.body.classList.add('dark-mode');
+      this.icon = this.iconSun;
+      // add dark mode to footer
+      document.getElementById('footer').classList.add('dark-mode-footer');
+      document.getElementById('resume').classList.add('dark-mode-resume');
+      
+      this.status = false
+    } else {
+      document.body.classList.remove('dark-mode');
+      this.icon = this.iconMoon;
+      // remove dark mode from footer
+      document.getElementById('footer').classList.remove('dark-mode-footer');
+      document.getElementById('resume').classList.remove('dark-mode-resume');
+      this.status = true
+    }
+  }
 }
